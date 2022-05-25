0d227afd5f6f2990c4bd73c9704a4401e2c0a474
Keny Debo <kenydebo@gmail.com>
2022-05-25 19:27:46 -0400

    Updated the organization and the workspace

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "ReadMe"
+    organization = "Omoba-Terraform-Demos"
 
     workspaces {
-      name = "Readme"
+      name = "demo-github-actions"
     }
   }
 }
